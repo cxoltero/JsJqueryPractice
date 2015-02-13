3319966dcb15bf40393ec106faa4dbdeb2c664fb
Roberto Hernandez <carlosh95@gmail.com>
2015-02-13 00:56:37 -0500

    app is almost ready, the functionality is there, answers open on event, and also close their siblings when one is open

---
+++ accordianSlider/js/main.js
@@ -0,0 +1,10 @@
+
+	var action= "click";
+	var speed = "500";
+$(document).ready(function(){
+	$("li.q").on(action, function(){
+		$(this).next().slideToggle(speed)
+			.siblings("li.a")
+				.slideUp();
+	});
+});
