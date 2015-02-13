3101563bd89c0e726f6f3ccee0b2ddc6fd88c99f
Roberto Hernandez <carlosh95@gmail.com>
2015-02-13 01:02:06 -0500

    this project is done as of now, functionality is at 100 %

--- accordianSlider/js/main.js
+++ accordianSlider/js/main.js
@@ -2,9 +2,17 @@
 	var action= "click";
 	var speed = "500";
 $(document).ready(function(){
+	// Get next element after question aka answer
 	$("li.q").on(action, function(){
+		// select all other answers
 		$(this).next().slideToggle(speed)
 			.siblings("li.a")
 				.slideUp();
+		// Get image for active question
+		var img = $(this).children('img');
+		// Remove the rotate class for all images except the active one
+		$('img').not(img).removeClass('rotate');
+		// Toggle roate class
+		img.toggleClass('rotate');
 	});
 });
